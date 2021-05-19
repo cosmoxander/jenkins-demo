66b530a5a783433ab2de3294b25060699e86d463
cosmoxander <cosmoxander@gmail.com>
2021-05-19 17:32:19 +0300

    init commit

---
+++ src/test/java/com/cosmo/xander/springj/SpringJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmo.xander.springj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
